3b560f1fc4ae34702fa0f9910b19aae9dfd2fb9b
rjonesca97@gmail.com <rjonesca97@gmail.com>
2014-01-25 22:50:19 +0000

    Working version of Page Retriever

---  tcss-422-os-project1/WebCrawler/src/edu/uw/tcss422/util/PageRetriever.java
+++  tcss-422-os-project1/WebCrawler/src/edu/uw/tcss422/util/PageRetriever.java
@@ -1,8 +1,8 @@
 package edu.uw.tcss422.util;
 
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
+import java.util.ArrayDeque;
+import java.util.HashMap;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -10,17 +10,18 @@ import org.jsoup.nodes.Document;
 public class PageRetriever {
  
   //Create collections to store pending links
-  private LinkedHashSet<String> mPendingLinks = new LinkedHashSet<String>();
-  private Iterator<String> mPendingLinksIt = null;
+  private ArrayDeque<String> mPendingLinks = new ArrayDeque<String>();
   
   //Create collections to store retrieved pages
-  private LinkedHashSet<Page> mAvailPages = new LinkedHashSet<Page>();
-  private Iterator<Page> mAvailPagesIt = mAvailPages.iterator();
+  private ArrayDeque<Page> mAvailPages = new ArrayDeque<Page>();
+  
+  //Page repository
+  private HashMap<String, Page> mPageRepos = new HashMap<String, Page>();
+  
   
   public PageRetriever(String aURL) {
     //Add initial link to queue to retrieve
     mPendingLinks.add(aURL);
-    mPendingLinksIt = mPendingLinks.iterator();
   }
   
   public void addURL(String aURL) {
@@ -29,27 +30,37 @@ public class PageRetriever {
   }
   
   public boolean hasNext() {
-    return mAvailPagesIt.hasNext();
+    return !mAvailPages.isEmpty();
   }
   
   public Page next() {
-    return mAvailPagesIt.next();
+    if( !mAvailPages.isEmpty() ) 
+      return mAvailPages.remove();
+    
+    return null; 
   }
   
   public void retrieve() {
-    while(mPendingLinksIt.hasNext()) {
-      final String url = mPendingLinksIt.next(); //Get next url
+    
+    while( !mPendingLinks.isEmpty() ) {
+      final String url = mPendingLinks.remove(); //Get next url
       
       if( url == null || url.isEmpty())
         return;
       
-      try {
-        //Connect to page and create page object
-        Document doc = Jsoup.connect(url).get();
-        String content = doc.html();
-        System.out.println(content);
-        //mAvailPages.add(new Page(url, content));
-      } catch( IOException e ) {}
+      //Check the repository first. If the Page has been retrieved before, return it; else retrieve the page and store for future.
+      if( mPageRepos.containsKey(url))
+        mAvailPages.add(mPageRepos.get(url));
+      else { 
+        try {
+          //Connect to page and create page object
+          Document doc = Jsoup.connect(url).get();
+          String content = doc.html();
+          Page page = new Page(url, content);
+          mAvailPages.add(page); //add retrieved page to avail queue
+          mPageRepos.put(url, page); //add page to repos
+        } catch( IOException e ) {}
+      }
     }
   }
 }
