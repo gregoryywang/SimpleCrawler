e1bbe296c8e3960c612d40889dd49a9a704dadcd
yongwang@uw.edu <yongwang@uw.edu@accd644c-cb3b-3ceb-26c2-9970fa2cb676>
2014-01-26 01:32:17 +0000

    Added more functionality. Still very much WIP though.

--- trunk/ tcss-422-os-project1/WebCrawler/src/edu/uw/tcss422/util/ParserTest.java
+++ trunk/ tcss-422-os-project1/WebCrawler/src/edu/uw/tcss422/util/ParserTest.java
@@ -1,6 +1,7 @@
 package edu.uw.tcss422.util;
 
 import java.io.IOException;
+import java.util.Collection;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -12,16 +13,26 @@ public class ParserTest {
 	    
 		PageParser parser = new PageParser();
 		
-		String url = "http://www.aol.com";   
+		String url = "http://www.aol.com"; 
+		
         Document doc = Jsoup.connect(url).get();
         
         parser.parse(doc);
 
         Elements links = parser.getLinks();
+        Collection<String> keywords = parser.getKeywords();
         
+        // Iterate over all links
         for (Element link : links) {
         	System.out.println(link.attr("abs:href"));
         }
+        
+        // Iterate over all keywords
+        for (String keyword : keywords){
+        	System.out.println(keyword);
+        }
+        
+    
 	}
 
 }
