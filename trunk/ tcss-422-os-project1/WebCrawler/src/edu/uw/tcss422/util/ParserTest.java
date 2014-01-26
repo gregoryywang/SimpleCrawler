4a3bcdfaae9c2e978f918c944a1fc085f1016095
yongwang@uw.edu <yongwang@uw.edu@accd644c-cb3b-3ceb-26c2-9970fa2cb676>
2014-01-26 07:05:29 +0000

    Implemented overloaded method parse for alternative ways to specify inputs to parse.

--- trunk/ tcss-422-os-project1/WebCrawler/src/edu/uw/tcss422/util/ParserTest.java
+++ trunk/ tcss-422-os-project1/WebCrawler/src/edu/uw/tcss422/util/ParserTest.java
@@ -20,7 +20,7 @@ public class ParserTest {
 		PageParser parser = new PageParser();
 		
 		Scanner scan = new Scanner(System.in);
-		System.out.print("Enter the URL address of the site you wish to parse: ");
+		System.out.print("Enter the URL address of the site you wish to parse (enter 1 to cheat): ");
 		String url = scan.nextLine(); 
 		if (url.equals("1")) {
 			url = "http://www.aol.com";
@@ -33,14 +33,15 @@ public class ParserTest {
         Elements links = parser.getLinks();
         Collection<String> words = parser.getWords();
         
+        System.out.println("Parsing complete. Here's a list of links found on this site:");
         // Iterate over all links
         for (Element link : links) {
         	System.out.println(link.attr("abs:href"));
         }
 
         // keyword search test
-        System.out.println("Word list for this page: " + words);
-        System.out.println("Enter the keyword you wish to search: ");
+        System.out.println("\nWord list for this page: " + words);
+        System.out.println("Enter the keyword you wish to search: [THIS FEATURE DOES NOT WORK CORRECTLY YET]");
         String keyword = scan.nextLine();
         System.out.print("Page contains the word " + keyword + ": " + words.contains(keyword));
     
