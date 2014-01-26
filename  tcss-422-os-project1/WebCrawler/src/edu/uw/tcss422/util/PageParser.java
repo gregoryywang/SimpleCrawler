9ecdfc992bdd7bfc9b697e219ce7bd444cd4672c
Yong Yu Wang <yongyuwang@users.noreply.github.com>
2014-01-26 01:37:46 +0000

    Added JavaDocs.

---  tcss-422-os-project1/WebCrawler/src/edu/uw/tcss422/util/PageParser.java
+++  tcss-422-os-project1/WebCrawler/src/edu/uw/tcss422/util/PageParser.java
@@ -10,12 +10,16 @@ import org.jsoup.select.Elements;
 
 public class PageParser {
 
-	// A collection of URLs parsed from Documents.
+	// A collection of URLs parsed from the Document.
 	private Elements links;
 	
-	// A collection of keywords parsed from Documents.
+	// A collection of keywords parsed from the Document.
 	Collection<String> keywords = new ArrayList<String>();
 	
+	/**
+	 * Parses an HTML Document object and extracts URLs and keywords.
+	 * @param doc the HTML document to parse
+	 */
 	public void parse(Document doc) {
 		
 		// Parse the document for URLs.
@@ -23,18 +27,17 @@ public class PageParser {
 		
 		// Add link text to list of keywords
 		for (Element link : links) {
-			if (link != null) {
-	            keywords.add(link.text());
-			}
-            
-            //DEBUG
+
+			keywords.add(link.text());
+			
+			//DEBUG
 			// System.out.println(link.text());
 
         }
 		
 		
 		// Add all links from this document into queue in PageRetriever. Collisions are handled by the retriever.
-		// ????
+		// ???? This functionality should be implemented in a controller class, probably WebCrawler.java
 		
 		/* DEBUG
 		System.out.printf("\nLinks: (%d)", links.size());
@@ -44,18 +47,32 @@ public class PageParser {
         */
 	}
 	
+	/**
+	 * Gets all links stored for current Document.
+	 * @return a list of links
+	 */
 	public Elements getLinks() {
 		return links;
 	}
 	
+	/**
+	 * Gets all keywords stored for current Document.
+	 * @return a list of keywords
+	 */
 	public Collection<String> getKeywords() {
 		return keywords;
 	}
 	
+	/**
+	 * Clears the URL list to parse another Document.
+	 */
 	public void clearLinks() {
 		links.clear();
 	}
 	
+	/**
+	 * Clears the keywords list to parse another Document.
+	 */
 	public void clearKeywords() {
 		keywords.clear();
 	}
