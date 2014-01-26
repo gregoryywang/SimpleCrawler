62a251008b452a78570b26493363e52db611d1c8
Yong Yu Wang <yongyuwang@users.noreply.github.com>
2014-01-26 01:32:17 +0000

    Added more functionality. Still very much WIP though.

---  tcss-422-os-project1/WebCrawler/src/edu/uw/tcss422/util/PageParser.java
+++  tcss-422-os-project1/WebCrawler/src/edu/uw/tcss422/util/PageParser.java
@@ -1,5 +1,8 @@
 package edu.uw.tcss422.util;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -10,10 +13,25 @@ public class PageParser {
 	// A collection of URLs parsed from Documents.
 	private Elements links;
 	
+	// A collection of keywords parsed from Documents.
+	Collection<String> keywords = new ArrayList<String>();
+	
 	public void parse(Document doc) {
 		
-		// parse the document for URLs
+		// Parse the document for URLs.
 		links = doc.select("a[href]");
+		
+		// Add link text to list of keywords
+		for (Element link : links) {
+			if (link != null) {
+	            keywords.add(link.text());
+			}
+            
+            //DEBUG
+			// System.out.println(link.text());
+
+        }
+		
 		
 		// Add all links from this document into queue in PageRetriever. Collisions are handled by the retriever.
 		// ????
@@ -28,6 +46,18 @@ public class PageParser {
 	
 	public Elements getLinks() {
 		return links;
+	}
+	
+	public Collection<String> getKeywords() {
+		return keywords;
+	}
+	
+	public void clearLinks() {
+		links.clear();
+	}
+	
+	public void clearKeywords() {
+		keywords.clear();
 	}
 	
 }
