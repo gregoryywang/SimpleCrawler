6f58467dafe5c71542be72988699770a92918a1a
Yong Yu Wang <yongyuwang@users.noreply.github.com>
2014-01-27 05:50:19 +0000

    Added methods and fields to Page class to support how PageParser saves data.

---  tcss-422-os-project1/WebCrawler/src/edu/uw/tcss422/util/Page.java
+++  tcss-422-os-project1/WebCrawler/src/edu/uw/tcss422/util/Page.java
@@ -1,8 +1,24 @@
 package edu.uw.tcss422.util;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.jsoup.select.Elements;
+
 public class Page {
   private String mURL;
   private String mContent;
+
+  /**
+   * A collection of URLs parsed from the this page by the PageRetriver.
+   */
+  private Elements links;
+
+  /**
+   * 	A collection of words parsed from this page by the PageRetriver.
+   */
+  private Collection<String> words = new ArrayList<String>();
+  
   
   public Page(final String aURL, final String aContent) {
     mURL = aURL;
@@ -35,6 +51,38 @@ public class Page {
    */
   public void setContent(String mContent) {
     this.mContent = mContent;
+  }
+
+  /**
+   * Sets the list of all links parsed from this page by the PageRetriver.
+   * @param a list of links
+   */
+  public void setLinks(Elements links) {
+	  this.links = links;
+  }
+
+  /**
+   * Sets the list of all words parsed from this page by the PageRetriver.
+   * @param a list of words
+   */
+  public void setWords(Collection<String> words) {
+	  this.words = words;
+  }
+  
+  /**
+   * Gets all links stored for this page.
+   * @return a list of links
+   */
+  public Elements getLinks() {
+	  return links;
+  }
+
+  /**
+   * Gets all words stored for this page.
+   * @return a list of words
+   */
+  public Collection<String> getWords() {
+	  return words;
   }
   
 }
---  tcss-422-os-project1/WebCrawler/src/edu/uw/tcss422/util/ParserTest.java
+++  tcss-422-os-project1/WebCrawler/src/edu/uw/tcss422/util/ParserTest.java
@@ -33,7 +33,7 @@ public class ParserTest {
         Elements links = parser.getLinks();
         Collection<String> words = parser.getWords();
         
-        System.out.println("Parsing complete. Here's a list of links found on this site:");
+        System.out.println("\nParsing complete. Here's a list of links found on this site:");
         // Iterate over all links
         for (Element link : links) {
         	System.out.println(link.attr("abs:href"));
@@ -43,7 +43,7 @@ public class ParserTest {
         System.out.println("\nWord list for this page: " + words);
         System.out.println("Enter the keyword you wish to search: [THIS FEATURE DOES NOT WORK CORRECTLY YET]");
         String keyword = scan.nextLine();
-        System.out.print("Page contains the word " + keyword + ": " + words.contains(keyword));
+        System.out.print("Page contains the word [" + keyword + "]: " + words.contains(keyword));
     
 	}
 
